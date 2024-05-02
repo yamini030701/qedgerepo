861960636c2a0d821f561666984614b281b06ca5
yamini030701 <yaminipriyalakshmi@gmail.com>
2024-05-02 20:39:04 +0530

    feature302 is added

--- Application.java
+++ Application.java
@@ -11,3 +11,15 @@ public class Main {
 // Outputs "I just got executed!"
  
 
+#feature302 branch new method is added
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();
+    myMethod();
+  }
+}
